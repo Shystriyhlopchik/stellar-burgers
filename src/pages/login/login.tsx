2e4937782745086bfe32b37a801bd2a8e301b8a5
dandreev <Azazello1992Kot!>
2026-03-26 23:01:36 +0300

    bag_8. Исправлен баг роутингом. После успешной авторизации пользователь автоматически перебрасывается на тот маршрут, на который он хотел попасть до авторизации.

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,6 +1,6 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { LoginUI } from '@ui-pages';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useDispatch } from '../../services/store';
 import { useSelector } from 'react-redux';
 import { loginUser, selectUserError } from '../../services/slices/authSlice';
@@ -11,8 +11,11 @@ export const Login: FC = () => {
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
 
   const errorText = useSelector(selectUserError);
+
+  const from = location.state?.from?.pathname || '/';
 
   const handleSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
@@ -25,7 +28,7 @@ export const Login: FC = () => {
     );
 
     if (loginUser.fulfilled.match(resultAction)) {
-      navigate('/');
+      navigate(from, { replace: true });
     }
   };
 
